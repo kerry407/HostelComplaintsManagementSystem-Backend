7e6ee2d51bc583cf6de6ad055ee3355fea7a00bb
Kerry Onyeogo <kerryonyeogo@gmail.com>
2023-07-08 12:34:39 +0100

    increased access token duration

--- HostelManagement/settings/base.py
+++ HostelManagement/settings/base.py
@@ -85,7 +85,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     # "ROTATE_REFRESH_TOKENS": True,
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=120),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1440),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=14),
 }
 
